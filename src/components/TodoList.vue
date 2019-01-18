51a7b2677a73d0551bc2918e83b06cae4598f6c7
mmahmud <mnm376@nyu.edu>
2019-01-18 10:59:33 -0500

    initial/final commit to repo

---
+++ src/components/TodoList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <!--//JavaScript expressions in Vue are enclosed in double curly brackets.-->
+    <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+    <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+    <todo v-on:delete-todo="deleteTodo" v-on:complete-todo="completeTodo" v-for="todo in todos" v-bind:todo="todo"></todo>
+  </div>
+</template>
+
+<script type = "text/javascript" >
+
+  import Todo from '@/components/Todo'
+  export default {
+    components: {Todo},
+    props: ['todos'],
+    methods: {
+      deleteTodo(todo) {
+        const todoIndex = this.todos.indexOf(todo);
+        this.todos.splice(todoIndex, 1);
+      },
+      completeTodo(todo) {
+        const todoIndex = this.todos.indexOf(todo);
+        this.todos[todoIndex].done = true;
+      }
+    }
+  };
+</script>
